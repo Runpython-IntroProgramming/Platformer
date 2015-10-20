2ee0b2bcd47cfd8c563627bba3b84f71fdc584c0
sarahdunbar <sarahdunbar@hanovernorwichschools.org>
2015-10-20 14:28:57 -0400

    Updated from Brython Server: 10/20/2015 2:28:57 PM

--- platformer.py
+++ platformer.py
@@ -15,7 +15,7 @@ SCREEN_HEIGHT = 1000
 class Player(Sprite):
     grassy = Color(0xeeff00, 1.0)
     thinline = LineStyle (1, grassy)
-    asset = RectangleAsset(5, 5, grassy, thinline)
+    asset = RectangleAsset(15, 45, thinline, grassy)
     def __init__(self, position):
         super().__init__(Player.asset, position)
         g = 1
@@ -26,40 +26,24 @@ class Player(Sprite):
         Sandbox.listenKeyEvent("keydown", "p", self.mouseClick)
         self.fxcenter = self.fycenter = 0.5
 
-    #def step(self):
-        #self.x += self.vx
-        #self.y += self.vy
-        #self.rotation += self.vr
-        #if self.thrust == 1:
-            #self.setImage(self.thrustframe)
-            #self.thrustframe += 1
-            #if self.thrustframe == 4:
-            #    self.thrustframe = 1
-        #else:
-            #self.setImage(0)
-            
-    def collidingWithSprites(self, sclass=None):
-        if sclass == None:
-            slist = App.spritelist
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
+        if self.thrust == 1:
+            self.setImage(self.thrustframe)
+            self.thrustframe += 1
+            if self.thrustframe == 4:
+                self.thrustframe = 1
         else:
-            slist = App.getSpritesbyClass(sclass)
-            t = 1
-            return t
-            #list(filter(self.collidingWith, slist))
+            self.setImage(0)
             
-    def mouseClick(self, event):
-        x = event.x
-        y = event.y
-        Player((x,y))
+        
 
     def Generate(self, event):
-        x = event.x
-        y = event.y
-        Player((x,y))
         t = 0
         while t == 0:
             self.vy += .1
-            t = collidingWithSprites(self, sclass=None)
 
 
 
@@ -71,7 +55,7 @@ class Sandbox(App):
         noline = LineStyle(0, black)
         bg_asset = RectangleAsset(width, height, noline, black)
         bg = Sprite(bg_asset, (0,0))
-        Player((30, 30))
+        Player((0, 0))
 
     def step(self):
         for x in self.getSpritesbyClass(Player):
