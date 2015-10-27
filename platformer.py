992c4ea843d3b134fb6831b99181bd8125622e5d
sarahdunbar <sarahdunbar@hanovernorwichschools.org>
2015-10-27 14:31:06 -0400

    Updated from Brython Server: 10/27/2015 2:31:06 PM

--- platformer.py
+++ platformer.py
@@ -61,7 +61,7 @@ class Player(Sprite):
         self.rotation += self.vr
         coll = len(self.collidingWithSprites())
         while coll == 1:
-            self.vy += .1
+            self.y += .1
             oldy = self.y
             coll = len(self.collidingWithSprites())
         self.x = oldx
