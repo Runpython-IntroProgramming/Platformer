2bab6c3b3beb80918a44a91f37f6c2e514fc92c4
dina-hertog <dinahertog-raz@hanovernorwichschools.org>
2015-11-12 14:55:53 -0500

    Updated from Brython Server: 11/12/2015 2:55:52 PM

--- platformer.py
+++ platformer.py
@@ -6,3 +6,11 @@ Assignment:
 Write and submit a program that implements the sandbox platformer game:
 https://github.com/HHS-IntroProgramming/Platformer
 """
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+
+black= (0x000000, 1.0)
+class Wall(Sprite):
+    wall = RectangleAsset(600, 400, thinline, skin)
+
+myapp = App()
+myapp.run()
