620b48bd7acf13a160ccbc7a7442d8e15a16e52f
dina-hertog <dinahertog-raz@hanovernorwichschools.org>
2015-11-13 14:14:14 -0500

    Updated from Brython Server: 11/13/2015 2:14:14 PM

--- platformer.py
+++ platformer.py
@@ -8,9 +8,16 @@ https://github.com/HHS-IntroProgramming/Platformer
 """
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
-black= (0x000000, 1.0)
+black= Color(0x000000, 1.0)
+red = Color(0xff0000,1.0)
+thinline = LineStyle(3, red)
+
 class Wall(Sprite):
-    wall = RectangleAsset(600, 400, thinline, skin)
+    wall = RectangleAsset(50, 50, thinline, black)
+    def __init__(self, xPos, yPos):
+        self.x = 100
+        self.y = 100
+Wall(400,300)
 
 myapp = App()
 myapp.run()
