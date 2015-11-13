ff660a8ef216c7a461eb59f90fb950f48b511f54
anoushkaalavilli <anoushkaalavilli@hanovernorwichschools.org>
2015-11-13 14:54:26 -0500

    Updated from Brython Server: 11/13/2015 2:54:26 PM

--- platformer.py
+++ platformer.py
@@ -9,19 +9,35 @@ https://github.com/HHS-IntroProgramming/Platformer
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 turquoise= Color(0x2EFEC8, 1.0)
+black= Color(0x000000, 1.0)
 
 thinline = LineStyle(1, black)
 noline= LineStyle(0, black)
 
-Block= RectangleAsset(500, 500, noline, turquoise)
-Sprite(block)
+block= RectangleAsset(125, 65, noline, turquoise)
+Sprite(block, (55, 250))
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
-class Block(Sprite):
-    asset = ImageAsset
+class Box3D(object):
+    def __init__(self, width, length, height):
+        self.w = width
+        self.l = length
+        self.h = height
 
+    def volume(self):
+        return self.w * self.l * self.h
+        
+box = Box3D(1,3,6)    # create a box object using the 
+                      # Box3D class or "blueprint"
+box2 = Box3D(3,6,2)   # create another, different object 
+                      # using the same class
+print(box.volume())
+print(box2.volume())  # volume() is a method of the Box3D class
+print(box.h)          # you can access the members
+box.h = 99.0          # and you can change them
+print(box.volume())   # and it will change the box properties
 
 
 myapp= App()
