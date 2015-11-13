511ae71acc32748ffb117814ca2f67b8e6e98f8e
jasminelou <yajielou@hanovernorwichschools.org>
2015-11-13 14:26:13 -0500

    Updated from Brython Server: 11/13/2015 2:26:12 PM

--- platformer.py
+++ platformer.py
@@ -6,4 +6,20 @@ Assignment:
 Write and submit a program that implements the sandbox platformer game:
 https://github.com/HHS-IntroProgramming/Platformer
 """
-print("Hello")
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+blue = Color(0x2EFEC8, 1.0)
+black = Color(0x000000, 1.0)
+
+thinline = LineStyle(1, blue)
+noline = LineStyle(0, blue)
+
+
+
+
+
+myapp = App()
+myapp.run()
