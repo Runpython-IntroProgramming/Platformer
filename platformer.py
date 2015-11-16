3a433acf6ca61d0aac51ba47d631fb72b18a937f
anoushkaalavilli <anoushkaalavilli@hanovernorwichschools.org>
2015-11-16 14:28:21 -0500

    Updated from Brython Server: 11/16/2015 2:28:21 PM

--- platformer.py
+++ platformer.py
@@ -14,30 +14,18 @@ black= Color(0x000000, 1.0)
 thinline = LineStyle(1, black)
 noline= LineStyle(0, black)
 
-block= RectangleAsset(125, 65, noline, turquoise)
-Sprite(block, (55, 250))
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
-class Box3D(object):
-    def __init__(self, width, length, height):
-        self.w = width
+class Box3D(Sprite):
+    block= RectangleAsset(125, 65, noline, turquoise)
+    def __init__(self, length, height):
+        super().__init__(Box3D.block, (length, height))
         self.l = length
         self.h = height
 
-    def volume(self):
-        return self.w * self.l * self.h
-        
-box = Box3D(1,3,6)    # create a box object using the 
-                      # Box3D class or "blueprint"
-box2 = Box3D(3,6,2)   # create another, different object 
-                      # using the same class
-print(box.volume())
-print(box2.volume())  # volume() is a method of the Box3D class
-print(box.h)          # you can access the members
-box.h = 99.0          # and you can change them
-print(box.volume())   # and it will change the box properties
+Box3D(55, 250)
 
 
 myapp= App()
