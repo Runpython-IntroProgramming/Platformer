dc4cf5754c6ba957ae478d69101758e163d2e324
jasminelou <yajielou@hanovernorwichschools.org>
2015-11-17 14:54:58 -0500

    Updated from Brython Server: 11/17/2015 2:54:58 PM

--- platformer.py
+++ platformer.py
@@ -8,27 +8,67 @@ https://github.com/HHS-IntroProgramming/Platformer
 """
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 480
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 1000
 
 blue = Color(0x2EFEC8, 1.0)
 black = Color(0x000000, 1.0)
+pink = Color(0xFF00FF, 1.0)
 
 thinline = LineStyle(1, blue)
 noline = LineStyle(0, blue)
 
 class Block(Sprite):
-    block = RectangleAsset(100, 25, thinline, blue)
+    block = RectangleAsset(50, 50, thinline, blue)
     def __init__(self, xval, yval):
         super().__init__(Block.block, (xval, yval))
         self.x = xval
         self.y = yval
+black = Color(0, 1)
+noline = LineStyle(0, black)
+bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
+bg = Sprite(bg_asset, (0,0))
 
 Block(100,400)
 
+class Char(Sprite):
+
+    char = RectangleAsset(25, 75, thinline, pink)
+
+    def __init__(self, x, y):
+        super().__init__(char.asset, position)
+        self.vx = 1
+        self.vy = 1
+        self.vr = 0.01
+        self.thrust = 0
+        self.thrustframe = 1
+        Platformer.listenKeyEvent("keydown", "space", self.thrustOn)
+        Platformer.listenKeyEvent("keyup", "space", self.thrustOff)
+        self.fxcenter = self.fycenter = 0.5
+
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
+        if self.thrust == 1:
+            self.setImage(self.thrustframe)
+            self.thrustframe += 1
+            if self.thrustframe == 4:
+                self.thrustframe = 1
+        else:
+            self.setImage(0)
+
+    def thrustOn(self, event):
+        self.thrust = 1
+
+    def thrustOff(self, event):
+        self.thrust = 0
+        
+Char(100,100)
+
 class Platformer(App):
-    def __init__(self):
+    def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT):
         super().__init__()
 
-myapp = Platformer()
+myapp = Platformer(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
