cb6df993999b6adc4d22dd1934f9ff666ec422f5
jasminelou <yajielou@hanovernorwichschools.org>
2015-11-18 13:55:18 -0500

    Updated from Brython Server: 11/18/2015 1:55:17 PM

--- platformer.py
+++ platformer.py
@@ -15,22 +15,24 @@ blue = Color(0x2EFEC8, 1.0)
 black = Color(0x000000, 1.0)
 pink = Color(0xFF00FF, 1.0)
 
-thinline = LineStyle(1, blue)
+thinline = LineStyle(2, pink)
 noline = LineStyle(0, blue)
 
 class Block(Sprite):
-    block = RectangleAsset(50, 50, thinline, blue)
+    block = RectangleAsset(40, 40, thinline, blue)
     def __init__(self, xval, yval):
         super().__init__(Block.block, (xval, yval))
         self.x = xval
         self.y = yval
+
 black = Color(0, 1)
-noline = LineStyle(0, black)
 bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
 bg = Sprite(bg_asset, (0,0))
 
 def buildBlock(event):
-    Block(event.x, event.y)
+    event.x = event.x - event.x%40
+    event.y = event.y - event.y%40
+    Block(event.x-10, event.y-10)
 
 class Platformer(App):
     def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT):
