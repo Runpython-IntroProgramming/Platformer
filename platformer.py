3eabb8234448d6936c6a6ba7b35d99dc2178ae86
dina-hertog <dinahertog-raz@hanovernorwichschools.org>
2015-11-18 11:34:08 -0500

    Updated from Brython Server: 11/18/2015 11:34:07 AM

--- platformer.py
+++ platformer.py
@@ -8,8 +8,11 @@ https://github.com/HHS-IntroProgramming/Platformer
 """
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
+ballX = imput(
+ball    
 black= Color(0x000000, 1.0)
 red = Color(0xff0000,1.0)
+blue= Color(0x0000ff, 1.0)
 thinline = LineStyle(3, red)
 
 class Wall(Sprite):
@@ -18,12 +21,16 @@ class Wall(Sprite):
         super().__init__(Wall.wall, (xPos, yPos))
         self.x = xPos
         self.y = yPos
+class Ball(Sprite):
+    ball = CircleAsset(30, thinline, blue)
+
+def moveChara(event):
+ Sprite(Ball, (event.x, event.y))
 
 def buildWall(event):
     #xPos.x = event.x
     #yPos.y = event.y
     Wall(event.x-25, event.y-25)
-
 
 class Platformer(App):
     def __init__(self):
@@ -32,5 +39,5 @@ class Platformer(App):
 
 myapp = Platformer()
 myapp.listenMouseEvent('click', buildWall)
-#myapp.listenMouseEvent('
+myapp.listenKeyEvent('keydown', 'space', moveChara)
 myapp.run()
