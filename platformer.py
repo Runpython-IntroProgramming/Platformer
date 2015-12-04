22f6ae73b18d0db9b14c7a1f13dcfbc52e40b6b7
jasminelou <yajielou@hanovernorwichschools.org>
2015-12-04 14:55:03 -0500

    Updated from Brython Server: 12/4/2015 2:55:02 PM

--- platformer.py
+++ platformer.py
@@ -87,7 +87,7 @@ class Platformer(App):
     def moveDogU(self, event):
         global gravity
         #self.dogsprite.y -= 50
-        gravity = -20
+        gravity = -7
         collisions = self.dogsprite.collidingWithSprites(Block)
         if collisions:
             self.dogsprite.y += 50
