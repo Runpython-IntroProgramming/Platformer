e41eb26f500dc4dbf15afc6e46c8c3d567371412
anoushkaalavilli <anoushkaalavilli@hanovernorwichschools.org>
2015-12-04 14:19:16 -0500

    Updated from Brython Server: 12/4/2015 2:19:15 PM

--- platformer.py
+++ platformer.py
@@ -80,20 +80,20 @@ def MoveCharacterRight (event):
     #Character.collidingWithSprites(Block)
     collision = charactersprite.collidingWithSprites(Block)
     if collision:
-        charactersprite.x -= 5
-        if charactersprite:
-            charactersprite.x -= 5
-    if charactersprite:
-        charactersprite.x += 5
-    
-def MoveCharacterLeft (event):
-    collision = charactersprite.collidingWithSprites(Block)
-    if collision:
         charactersprite.x += 5
         if charactersprite:
             charactersprite.x += 5
     if charactersprite:
         charactersprite.x -= 5
+    
+def MoveCharacterLeft (event):
+    collision = charactersprite.collidingWithSprites(Block)
+    if collision:
+        charactersprite.x -= 5
+        if charactersprite:
+            charactersprite.x -= 5
+    if charactersprite:
+        charactersprite.x += 5
     
 def MoveCharacterUp (event):
     collision = charactersprite.collidingWithSprites(Block)
