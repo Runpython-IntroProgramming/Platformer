f08cea778fda6d98987abfefe8b5b5caa187055e
anoushkaalavilli <anoushkaalavilli@hanovernorwichschools.org>
2015-12-08 14:42:12 -0500

    Updated from Brython Server: 12/8/2015 2:42:12 PM

--- platformer.py
+++ platformer.py
@@ -125,7 +125,7 @@ class Platformer(App):
         if charactersprite:
             collision = charactersprite.collidingWithSprites(Block)
             if not collision:
-                gravity += 0.2
+                gravity += 0.25
                 charactersprite.y += gravity
         
 myapp= Platformer(SCREEN_WIDTH, SCREEN_HEIGHT)
