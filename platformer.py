34b47c4427d53e1ebcd465700830bf5c0c94e9ba
sharkfinn23 <thomashackett@hanovernorwichschools.org>
2016-10-24 15:35:19 -0400

    Updated from Brython Server: 10/24/2016 3:35:18 PM

--- platformer.py
+++ platformer.py
@@ -20,41 +20,33 @@ grey = Color(0x000000, 0.5)
 #grid
 gridline = LineStyle(1, grey)
 grid=RectangleAsset(30,30,gridline,white)
-Sprite(grid, (0,0))
 width=list(range(0,41))
 height=list(range(0,41))
-
+a=2
 #display grid
-for x in width:
-    for y in height:
-        Sprite(grid, (20*x,20*y))
+
 
 #create wall
 class wall(Sprite):
     wall = RectangleAsset(20,20,gridline,black)
     def __init__(self, position):
         super().__init__(wall.asset, position)
-        wall.listenKeyEvent("keydown", "w", self.wallcreate)
-        wall.listenKeyEvent("keyup", "w", self.walldontcreate)
-        
+        listenKeyEvent("keydown", "w", platformer.wallcreate)
+        listenKeyEvent("keyup", "w", platformer.walldontcreate)
+
+    
+    
+   
+    
+class platformer(app):
+    for x in width:
+        for y in height:
+            Sprite(grid, (20*x,20*y))
 
     def wallcreate(self, event):
-        x=1
-    
+        a=1
     def walldontcreate(self, event):
-        x=0
-    
-class Platformer(App):
-    def __init__(self, width, height):
-        super().__init__(width, height)
-        
-    def step(self):
-        print(hi)
-        if wallcreate == 1:
-            Sprite(wall,(0,0))
-        else:
-            Sprite(wall,(100,100))
-            
-
-myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
+        a=0
+print(a)
+myapp = Platformer(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
