b32ff47bdb4e92db7630b671e0ee73e3229b065c
KraweDad <mattfenner35@gmail.com>
2016-10-25 13:54:36 -0400

    Updated from Brython Server: 10/25/2016 1:54:36 PM

--- platformer.py
+++ platformer.py
@@ -1,8 +1,32 @@
 """
 platformer.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Matthew F
+Credit: Robbie
 Assignment:
 Write and submit a program that implements the sandbox platformer game:
 https://github.com/HHS-IntroProgramming/Platformer
 """
+
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+class Platformer(App):
+    """
+    Tutorial4 space game example.
+    """
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        black = Color(1, 0)
+        Black = Color(0, 1)
+        Blue = Color(0x0000ff, 1.0)
+        noline = LineStyle(.25, Black)
+        Noline = LineStyle(0, Black)
+        bg_asset = RectangleAsset(width, height, noline, black)
+        bg = Sprite(bg_asset, (0,0))
+        Boxy_asset = RectangleAsset(25, 50, Noline, Blue)
+        Sprite(Boxy_asset)
+
+myapp = Platformer(SCREEN_WIDTH, SCREEN_HEIGHT)
+myapp.run()
