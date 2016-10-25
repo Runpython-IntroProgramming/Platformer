2abc844314e06a058a22eada5d273cc0cbc5b330
KraweDad <mattfenner35@gmail.com>
2016-10-25 14:01:31 -0400

    Updated from Brython Server: 10/25/2016 2:01:31 PM

--- platformer.py
+++ platformer.py
@@ -12,21 +12,25 @@ from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Fra
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
+class Boxy(sprite)
+    """
+    THIS IS BOXY
+    """
+    
 class Platformer(App):
     """
-    Tutorial4 space game example.
+    THIS IS BOXY'S WORLD
     """
     def __init__(self, width, height):
         super().__init__(width, height)
         black = Color(1, 0)
         Black = Color(0, 1)
         Blue = Color(0x0000ff, 1.0)
-        noline = LineStyle(.25, Black)
+        noline = LineStyle(5, Black)
         Noline = LineStyle(0, Black)
         bg_asset = RectangleAsset(width, height, noline, black)
         bg = Sprite(bg_asset, (0,0))
-        Boxy_asset = RectangleAsset(25, 50, Noline, Blue)
-        Sprite(Boxy_asset)
+        
 
 myapp = Platformer(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
