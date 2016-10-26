3836835489b7e5b8f177eae36966c8e53b679f15
sharkfinn23 <thomashackett@hanovernorwichschools.org>
2016-10-26 10:11:29 -0400

    Updated from Brython Server: 10/26/2016 10:11:29 AM

--- platformer.py
+++ platformer.py
@@ -10,7 +10,7 @@ https://github.com/HHS-IntroProgramming/Platformer
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 SCREEN_WIDTH=801
-SCREEN_HEIGHT=801
+SCREEN_HEIGHT=601
 red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
@@ -19,6 +19,7 @@ white = Color(0xffffff, 1.0)
 grey = Color(0x000000, 0.5)
 
 #grid
+greenline = LineStyle(1, green)
 gridline = LineStyle(1, grey)
 grid=RectangleAsset(30,30,gridline,white)
 width=list(range(0,41))
@@ -26,11 +27,12 @@ height=list(range(0,41))
 a=2
 #display grid
 wall = RectangleAsset(20,20,gridline,black)
+dude = RectangleAsset(10,20,greenline,green)
 mousepositionx=0
 mousepositiony=0
 a=0
 b=0
-    
+
    
 
 for x in width:
@@ -38,28 +40,33 @@ for x in width:
         Sprite(grid, (20*x,20*y))
     
 
-
+def pKey(even):
+    global a
+    global b
+    Sprite(dude, (a, b))
+    
 def drag(event):
     global mousepositionx
     global mousepositiony
     global a
     global b
-    a=event.x%20
-    b=event.y%20
-    mousepositionx=(event.x-a)
-    mousepositiony=(event.y-b)
-    print("a")
+    a=event.x
+    b=event.y
+    mousepositionx=(event.x - event.x%20)
+    mousepositiony=(event.y- event.y%20)
 
 def wKey(event):
     global mousepositionx
     global mousepositiony
     Sprite(wall, (mousepositionx, mousepositiony))
 
+
+
 myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
 # Set up event handlers for the app
 myapp.listenKeyEvent('keydown', 'w', wKey)
 myapp.listenMouseEvent('mousemove', drag)
-
+myapp.listenKeyEvent('keydown', 'p', pKey)
 
 
 myapp.run()
