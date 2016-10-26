ca04a25259a42570df17fab9e3d00ea4ada0d765
KraweDad <mattfenner35@gmail.com>
2016-10-26 13:16:06 -0400

    Updated from Brython Server: 10/26/2016 1:16:05 PM

--- platformer.py
+++ platformer.py
@@ -5,6 +5,11 @@ Credit: Robbie
 Assignment:
 Write and submit a program that implements the sandbox platformer game:
 https://github.com/HHS-IntroProgramming/Platformer
+        Platformer.listenKeyEvent("keydown", "s", self.SPRING)
+        Platformer.listenKeyEvent("keyup", "s", self.SPRINGoff)
+            
+        Platformer.listenKeyEvent("keydown", "w", self.WALL)
+        Platformer.listenKeyEvent("keyup", "w", self.WALLoff)
 """
 
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
@@ -12,11 +17,45 @@ from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Fra
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
-class Boxy(sprite)
+class Wall(Sprite):
+    black = Color(0, 1)
+    noline = LineStyle(0,black)
+    asset = RectangleAsset(25, 25, noline, black)
+
+
+
+class Boxy(Sprite):
     """
     THIS IS BOXY
     """
+    Black = Color(0, 1)
+    oline = LineStyle(0, Black)
+    blue = Color(0x0000ff, 1.0)
+    asset = RectangleAsset(25, 50, oline, blue)
     
+    def __init__(self, position):
+        super().__init__(Boxy.asset, position)
+        self.vx = 0
+        self.vy = 0
+        self.fxcenter = self.fycenter = 0.25
+        Platformer.listenKeyEvent("keydown", "d", self.moveright)
+        Platformer.listenKeyEvent("keyup", "d", self.moveoff)
+        Platformer.listenKeyEvent("keydown", "a", self.moveleft)
+        Platformer.listenKeyEvent("keyup", "a", self.moveoff)
+
+        
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+    
+    def moveoff(self,event):
+        self.vx = self.vy = 0
+    def moveright(self, event):
+        self.vx = 5 
+    def moveleft(self,event):
+        self.vx = -5
+    
+        
 class Platformer(App):
     """
     THIS IS BOXY'S WORLD
@@ -30,7 +69,11 @@ class Platformer(App):
         Noline = LineStyle(0, Black)
         bg_asset = RectangleAsset(width, height, noline, black)
         bg = Sprite(bg_asset, (0,0))
+        Boxy((1,1))
         
-
+    def step(self):
+        for ship in self.getSpritesbyClass(Boxy):
+            ship.step()
+        
 myapp = Platformer(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
