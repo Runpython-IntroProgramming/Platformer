be273638c958037b7cfb24f20251bb7430c018bf
Aqkotz <andy.lyme@mac.com>
2016-11-03 13:06:54 -0400

    Updated from Brython Server: 11/3/2016 1:06:54 PM

--- platformer.py
+++ platformer.py
@@ -10,26 +10,27 @@ https://github.com/HHS-IntroProgramming/Platformer
 from ggame import App, RectangleAsset, Sprite
 from ggame import LineStyle, Color
 
-black = Color(0x000000, 1)
-green = Color(0x00ff00, 1)
-white = Color(0xFFFFFF, 1)
+black = Color(0x000000, 1.0)
+green = Color(0x00ff00, 1.0)
+white = Color(0xFFFFFF, 1.0)
 
 thinline = LineStyle(1, black)
-wallplace = RectangleAsset(5, 5, thinline, black)
+wallplace = RectangleAsset(25, 25, thinline, black)
 
 SCREEN_WIDTH = 320
 SCREEN_HEIGHT = 240
 
 
-listx= list(range(64))
-for i in listx:
-    i = i+1
-    i = i*5
-listy= list(range(48))
-for j in listy:
-    j = j+1
-    j = 5*j
-
+listxs= list(range(1, 65))
+listx = []
+for i in listxs:
+    x = i*25
+    listx.append(x)
+listys= list(range(1, 49))
+listy = []
+for j in listys:
+    y = 25*j
+    listy.append(y)
 
 def closestx(listx, numclickx):
     myestimation=[]
@@ -62,8 +63,9 @@ def closestx(listx, numclickx):
         lowestneg = myestneg[(len(myestneg)-1)]
         final=lowestneg
     final = final + numclickx
-    return(final)
-def closesty(listy, numclicky):
+    return (final)
+    print (final)
+def closesty(listx, numclicky):
     myestimation=[]
     myestimationordered=[]
     myvar=0
@@ -94,7 +96,9 @@ def closesty(listy, numclicky):
         lowestneg = myestneg[(len(myestneg)-1)]
         final=lowestneg
     final = final + numclicky
-    return(final)
+    return (final)
+    print(final)
+    
     
 def mouseClick(event):
     numclickx = event.x
@@ -102,53 +106,12 @@ def mouseClick(event):
     xcoord = closestx(listx, numclickx)
     ycoord = closesty(listy, numclicky)
     Sprite (wallplace, (xcoord, ycoord))
-    print (event.x)
-    print (event.y)
+    print (str(event.x) + " = mouseclickX")
+    print (str(event.y) + " = mouseclickY")
     print (closestx(listx, event.x))
     print (closesty(listy, event.y))
 
 myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.listenMouseEvent('click', mouseClick)
 
-myapp.run
-
-
-numclicky = int(input("input"))
-listys= list(range(1,49))
-listy=[]
-for j in listys:
-    x = j*5
-    listy.append(x)
-
-myestimation=[]
-myestimationordered=[]
-myvar=0
-while myvar < len(listy):
-    myestimation.append(listy[myvar]-numclicky)
-    myvar+=1
-myvar=0
-myestimationordered = myestimation
-myestneg=[]
-for i in myestimationordered:
-    if i<=0:
-        myestneg.append(i)
-for j in range(10):
-    for i in myestimationordered:
-        if i<=0:
-            myestimationordered.remove(i)
-if len(myestimationordered) > 0 and len(myestneg) > 0:
-    lp = myestimationordered[0]
-    ln = myestneg[len(myestneg)-1]
-    if abs(ln)<abs(lp):
-        final=ln
-    elif abs(lp)<abs(ln):
-        final=lp    
-elif len(myestimationordered) > 0:
-    lowestpos = myestimationordered[0]
-    final=lowestpos
-elif len(myestneg) >0:
-    lowestneg = myestneg[(len(myestneg)-1)]
-    final=lowestneg
-final = final + numclicky
-print (final)
-
+myapp.run()
