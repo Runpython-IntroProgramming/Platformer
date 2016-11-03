582cdfc7557807bc9880b141cd3fb4a5d97fc8dc
Aqkotz <andy.lyme@mac.com>
2016-11-03 13:40:27 -0400

    Updated from Brython Server: 11/3/2016 1:40:27 PM

--- platformer.py
+++ platformer.py
@@ -20,96 +20,100 @@ wallplace = RectangleAsset(25, 25, thinline, black)
 SCREEN_WIDTH = 1300
 SCREEN_HEIGHT = 975
 
+#place wall sprites & snap to grid
+for j in range(1):
+    #make lists
+    for i in range(1):
+        listxs= list(range(1, 129))
+        listx = []
+        for i in listxs:
+            x = i*25
+            listx.append(x)
+        listys= list(range(1, 97))
+        listy = []
+        for j in listys:
+            y = 25*j
+            listy.append(y)
 
-listxs= list(range(1, 129))
-listx = []
-for i in listxs:
-    x = i*25
-    listx.append(x)
-listys= list(range(1, 97))
-listy = []
-for j in listys:
-    y = 25*j
-    listy.append(y)
-
-def closestx(listx, numclickx):
-    myestimation=[]
-    myestimationordered=[]
-    myvar=0
-    while myvar < len(listx):
-        myestimation.append(listx[myvar]-numclickx)
-        myvar+=1
-    myvar=0
-    myestimationordered = myestimation
-    myestneg=[]
-    for i in myestimationordered:
-        if i<=0:
-            myestneg.append(i)
-    for j in range(10):
+    #make closest functions
+    def closestx(listx, numclickx):
+        myestimation=[]
+        myestimationordered=[]
+        myvar=0
+        while myvar < len(listx):
+            myestimation.append(listx[myvar]-numclickx)
+            myvar+=1
+        myvar=0
+        myestimationordered = myestimation
+        myestneg=[]
         for i in myestimationordered:
             if i<=0:
-                myestimationordered.remove(i)
-    if len(myestimationordered) > 0 and len(myestneg) > 0:
-        lp = myestimationordered[0]
-        ln = myestneg[len(myestneg)-1]
-        if abs(ln)<abs(lp):
-            final=ln
-        elif abs(lp)<abs(ln):
-            final=lp    
-    elif len(myestimationordered) > 0:
-        lowestpos = myestimationordered[0]
-        final=lowestpos
-    elif len(myestneg) >0:
-        lowestneg = myestneg[(len(myestneg)-1)]
-        final=lowestneg
-    final = final + numclickx
-    return (final)
-    print (final)
-def closesty(listx, numclicky):
-    myestimation=[]
-    myestimationordered=[]
-    myvar=0
-    while myvar < len(listy):
-        myestimation.append(listy[myvar]-numclicky)
-        myvar+=1
-    myvar=0
-    myestimationordered = myestimation
-    myestneg=[]
-    for i in myestimationordered:
-        if i<=0:
-            myestneg.append(i)
-    for j in range(10):
+                myestneg.append(i)
+        for j in range(10):
+            for i in myestimationordered:
+                if i<=0:
+                    myestimationordered.remove(i)
+        if len(myestimationordered) > 0 and len(myestneg) > 0:
+            lp = myestimationordered[0]
+            ln = myestneg[len(myestneg)-1]
+            if abs(ln)<abs(lp):
+                final=ln
+            elif abs(lp)<abs(ln):
+                final=lp    
+        elif len(myestimationordered) > 0:
+            lowestpos = myestimationordered[0]
+            final=lowestpos
+        elif len(myestneg) >0:
+            lowestneg = myestneg[(len(myestneg)-1)]
+            final=lowestneg
+        final = final + numclickx
+        return (final)
+        print (final)
+    def closesty(listx, numclicky):
+        myestimation=[]
+        myestimationordered=[]
+        myvar=0
+        while myvar < len(listy):
+            myestimation.append(listy[myvar]-numclicky)
+            myvar+=1
+        myvar=0
+        myestimationordered = myestimation
+        myestneg=[]
         for i in myestimationordered:
             if i<=0:
-                myestimationordered.remove(i)
-    if len(myestimationordered) > 0 and len(myestneg) > 0:
-        lp = myestimationordered[0]
-        ln = myestneg[len(myestneg)-1]
-        if abs(ln)<abs(lp):
-            final=ln
-        elif abs(lp)<abs(ln):
-            final=lp    
-    elif len(myestimationordered) > 0:
-        lowestpos = myestimationordered[0]
-        final=lowestpos
-    elif len(myestneg) >0:
-        lowestneg = myestneg[(len(myestneg)-1)]
-        final=lowestneg
-    final = final + numclicky
-    return (final)
-    print(final)
+                myestneg.append(i)
+        for j in range(10):
+            for i in myestimationordered:
+                if i<=0:
+                    myestimationordered.remove(i)
+        if len(myestimationordered) > 0 and len(myestneg) > 0:
+            lp = myestimationordered[0]
+            ln = myestneg[len(myestneg)-1]
+            if abs(ln)<abs(lp):
+                final=ln
+            elif abs(lp)<abs(ln):
+                final=lp    
+        elif len(myestimationordered) > 0:
+            lowestpos = myestimationordered[0]
+            final=lowestpos
+        elif len(myestneg) >0:
+            lowestneg = myestneg[(len(myestneg)-1)]
+            final=lowestneg
+        final = final + numclicky
+        return (final)
+        print(final)
     
-    
-def mouseClick(event):
-    numclickx = event.x-15
-    numclicky = event.y-15
-    xcoord = closestx(listx, numclickx)
-    ycoord = closesty(listy, numclicky)
-    Sprite (wallplace, (xcoord, ycoord))
-    print (str(event.x) + " = mouseclickX")
-    print (str(event.y) + " = mouseclickY")
-    print (closestx(listx, event.x))
-    print (closesty(listy, event.y))
+    #make wall sprites   
+    def mouseClick(event):
+        numclickx = event.x-15
+        numclicky = event.y-15
+        xcoord = closestx(listx, numclickx)
+        ycoord = closesty(listy, numclicky)
+        Sprite (wallplace, (xcoord, ycoord))
+        print (str(event.x) + " = mouseclickX")
+        print (str(event.y) + " = mouseclickY")
+        print (closestx(listx, event.x))
+        print (closesty(listy, event.y))
 
 myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.listenMouseEvent('click', mouseClick)
