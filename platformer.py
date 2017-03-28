926495fd5f45a9fb443fee5783f787b8c90d821e
jasper-meyer <jaspermeyer@hanovernorwichschools.org>
2017-03-28 14:44:15 -0400

    Updated from Brython Server: 3/28/2017 2:44:13 PM

--- platformer.py
+++ platformer.py
@@ -1,8 +1,55 @@
 """
 platformer.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Jasper Meyer
+Credit: Will, you, the internet, Brendan
 Assignment:
 Write and submit a program that implements the sandbox platformer game:
 https://github.com/HHS-IntroProgramming/Platformer
 """
+
+
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+
+SCREEN_WIDTH = 1080
+SCREEN_HEIGHT = 720
+
+black = Color(0, 1)
+backcol = Color(0xd9ffcc, 1.0)
+noline = LineStyle(0, black)
+bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, backcol)
+bg = Sprite(bg_asset, (0,0))
+
+sq = Sprite(rectangle, (200, 50))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
+myapp.run()
