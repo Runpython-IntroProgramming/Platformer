c182babc3b8e4c8564c53f1f784af1f995be3319
jasper-meyer <jaspermeyer@hanovernorwichschools.org>
2017-04-04 14:13:22 -0400

    Updated from Brython Server: 4/4/2017 2:13:22 PM

--- platformer.py
+++ platformer.py
@@ -15,16 +15,19 @@ SCREEN_HEIGHT = 720
 myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
 black = Color(0, 1)
 backcol = Color(0xd9ffcc, 1.0)
+purp = Color(0x9900cc, 1.0)
 noline = LineStyle(0, black)
 bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, backcol)
 bg = Sprite(bg_asset, (0,0))
 thinline = LineStyle(1, black)
 sq = RectangleAsset (75,75, noline, black)
 wub=0
+pup=0
 mousex=0
 mousey=0
 mousexround=0
 mouseyround=0
+play = RectangleAsset (75,75, noline, purp)
 
 def wup(event):
     global wub
@@ -43,6 +46,16 @@ def mousemo(event):
     mousey=event.y
     
 
+def pup(event):
+    global pub
+    global mousexround
+    global mouseyround
+    pub = 1
+    if pub == 1:
+        mousexround=mousex-((mousex)%75)
+        mouseyround=mousey-((mousey)%75)
+        block = Sprite (sq, (mousexround, mouseyround))
+
 
 
     
@@ -76,7 +89,7 @@ def mousemo(event):
 
 
 
-#myapp.listenKeyEvent('keyup', 'p', pup)
+myapp.listenKeyEvent('keyup', 'p', pup)
 myapp.listenKeyEvent('keyup', 'w', wup)
 myapp.listenMouseEvent('mousemove', mousemo)
 
