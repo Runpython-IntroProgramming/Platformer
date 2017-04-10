288c03a8b93e10a5f17d334a15e992ff38e0690f
sharkfinn23 <thomashackett@hanovernorwichschools.org>
2017-04-10 14:24:37 -0400

    Updated from Brython Server: 4/10/2017 2:24:37 PM

--- platformer.py
+++ platformer.py
@@ -1,16 +1,21 @@
 """
 platformer.py
-Author: Finn
-Credit: <list sources used, if any>
+Author: Jasmine Lou
+Credit: Classmates, Mr. Dennison, ggame documentation
 Assignment:
 Write and submit a program that implements the sandbox platformer game:
 https://github.com/HHS-IntroProgramming/Platformer
 """
-#imports
-from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset, Frame
 
-SCREEN_WIDTH=801
-SCREEN_HEIGHT=601
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+blue = Color(0x2EFEC8, 1.0)
+black = Color(0x000000, 1.0)
+pink = Color(0xFF00FF, 1.0)
+red = Color(0xFF5733, 1.0)
+white = Color(0xFFFFFF, 1.0)
 red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
@@ -18,125 +23,102 @@ black = Color(0x000000, 1.0)
 white = Color(0xffffff, 1.0)
 grey = Color(0x000000, 0.5)
 
-#grid
+thinline = LineStyle(2, black)
+blkline = LineStyle(1, black)
+noline = LineStyle(0, white)
+blueline = LineStyle(2, blue)
+redline = LineStyle(1, red)
 greenline = LineStyle(1, green)
 gridline = LineStyle(1, grey)
 grid=RectangleAsset(30,30,gridline,white)
-width=list(range(0,41))
-height=list(range(0,41))
-a=2
 
+black = Color(0, 1)
+bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
+bg = Sprite(bg_asset, (0,0))
 
-mousepositionx=0
-mousepositiony=0
-a=0
-b=0
-dudesprite = None
-wallsprite = None
-
-   
-for x in width:
-    for y in height:
-        Sprite(grid, (20*x,20*y))
-
-
+class Wall(Sprite):
+    wall = RectangleAsset(40, 40, thinline, blue)
+    def __init__(self, x, y):
+        super().__init__(Wall.wall, (x, y))
+        self.x = x
+        self.y = y
 
 class Dude(Sprite):
-    dude = RectangleAsset(10,20,greenline,green)
-    def __init__(self, a, b):
-        super().__init__(Dude.dude, (a, b))
+    dude = RectangleAsset(30, 30, coolline, pink)
+    def __init__(self, x, y):
+        super().__init__(Dude.dude, (x, y))
         self.x = x
-        self.y = x
+        self.y = y
+        
     
     def step(self):
         self.gravity += 0.3
         self.y += self.gravity
-        dudecollisions = self.collidingWithSprites(Wall)
-        if dudecollisions:
+        scollisions = self.collidingWithSprites(wall)
+        if scollisions:
             self.y -= self.gravity
             self.gravity = 0
-        
-class Wall(Sprite):
-    wall = RectangleAsset(20,20,gridline,black)
-    def __init__(self, a, b):
-        super().__init__(Wall.wall, (a, b))
-        self.x = a
-        self.y = b
 
+gravity = 0
 
-
-gravity=0
-
-
-
-
-
-
-class Game(App):
+class Platformer(App):
     def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT):
         super().__init__()
-        a = 0
-        b = 0
+        self.mousex = 0
+        self.mousey = 0
+        self.dude = 0
         self.dudesprite = None
-        self.listenKeyEvent('keydown', 'w', self.classwall)
-        self.listenMouseEvent('mousemove', self.drag)
-        self.listenKeyEvent('keydown', 'p', self.classdude)
-        self.listenKeyEvent('keydown', 'right arrow', self.Right)
-        self.listenKeyEvent('keydown', 'left arrow', self.Left)
-        self.listenKeyEvent('keydown', 'up arrow', self.Jump)
-
-
-    def drag(self, event):
-        global mousepositionx
-        global mousepositiony
-        global a
-        global b
-        a=event.x
-        b=event.y
-        mousepositionx=(event.x - event.x%20)
-        mousepositiony=(event.y- event.y%20)
-
-    def Right(self,event):
-        global a
-        global b
-        c=0
-        while c != 5:
-            self.dudesprite.a += 1
-            bump = self.dudesprite.collidingWithSprites(Wall)
-            if bump:
-                self.dudesprite.a -= 1
-            c = c + 1
-                
-    def Left(self,event):
-        global a
-        global b
-        while c != 5:
-            self.dudesprite.a -= 1
-            bump = self.dudesprite.collidingWithSprites(Wall)
-            if bump:
-                self.dudesprite.a += 5
-       
-    def Jump(self,event):
-        global a
-        global b
+        self.listenKeyEvent('keydown', 'p', self.buildDude)
+        self.listenKeyEvent('keydown', 'w', self.buildWall)
+        self.listenMouseEvent('mousemove', self.motion)
+        self.listenKeyEvent('keydown', 'right arrow', self.moveDudeR)
+        self.listenKeyEvent('keydown', 'left arrow', self.moveDudeL)
+        self.listenKeyEvent('keydown', 'up arrow', self.moveDudeU)
+        self.listenKeyEvent('keydown', 'down arrow', self.moveDudeD)
+    
+    def motion(self, event):
+        self.mousex = event.x
+        self.mousey = event.y
+    
+    def buildDude (self, event):
+        global gravity
+        if self.dudesprite:
+            self.dudesprite.destroy()
+            gravity = 0
+        self.dudesprite = Dude(self.mousex - 15, self.mousey - 15)
+    
+    def buildWall(self, event):
+        x = self.mousex - self.mousex%40
+        y = self.mousey - self.mousey%40
+        Wall(x-10, y-10)
+        
+        
+    def moveDudeR(self, event):
+        self.dudesprite.x += 5
+        collisions = self.dudesprite.collidingWithSprites(Wall)
+        if collisions:
+            self.dudesprite.x -= 5
+            
+    def moveDudeL(self, event):
+        self.dudesprite.x -= 5
+        collisions = self.dudesprite.collidingWithSprites(Wall)
+        if collisions:
+            self.dudesprite.x += 5
+            
+    def moveDudeU(self, event):
         global gravity
         if gravity == 0:
             gravity = -7
-        if dudesprite:
-            gravity = -5
+            collisions = self.dudesprite.collidingWithSprites(Wall)
+            if collisions:
+                self.dudesprite.y += 50
 
-
-    def classwall(self, event):
-        gravity=0
-        global a, b, wallsprite
-        wallsprite = Wall(a-a%20, b-b%20)
-
-    def classdude(self,event):
-        gravity = 0
-        global a, b, dudesprite
-        dudesprite = Dude(a, b)
- 
-
+    def moveDudeD(self, event):
+        self.dudesprite.y += 5
+        collisions = self.dudesprite.collidingWithSprites(Wall)
+        if collisions:
+            self.dudesprite.y -= 5
+            
     def step(self):
         global gravity
         if self.dudesprite:
@@ -144,9 +126,8 @@ class Game(App):
             self.dudesprite.y += gravity
             collisions = self.dudesprite.collidingWithSprites(Wall)
             if collisions:
-                self.dogsprite.y -= gravity
+                self.dudesprite.y -= gravity
                 gravity = 0
-                
-
-myapp = Game(SCREEN_WIDTH, SCREEN_HEIGHT)
+            
+myapp = Platformer(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
