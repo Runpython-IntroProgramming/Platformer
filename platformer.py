2697c0c385729f34dc8a0501f902313587e9ca1d
sharkfinn23 <thomashackett@hanovernorwichschools.org>
2017-04-11 14:14:41 -0400

    Updated from Brython Server: 4/11/2017 2:14:42 PM

--- platformer.py
+++ platformer.py
@@ -1,7 +1,7 @@
 """
 platformer.py
-Author: Jasmine Lou
-Credit: Classmates, Mr. Dennison, ggame documentation
+Author: Finn Hackett
+Credit: 
 Assignment:
 Write and submit a program that implements the sandbox platformer game:
 https://github.com/HHS-IntroProgramming/Platformer
@@ -21,7 +21,7 @@ green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 white = Color(0xffffff, 1.0)
-grey = Color(0x000000, 0.5)
+grey = Color(0xC0C0C0, 1.0)
 
 thinline = LineStyle(2, black)
 blkline = LineStyle(1, black)
@@ -38,14 +38,14 @@ bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
 bg = Sprite(bg_asset, (0,0))
 
 class Wall(Sprite):
-    wall = RectangleAsset(40, 40, thinline, blue)
+    wall = RectangleAsset(40, 40, thinline, grey)
     def __init__(self, x, y):
         super().__init__(Wall.wall, (x, y))
         self.x = x
         self.y = y
 
 class Dude(Sprite):
-    dude = RectangleAsset(30, 30, coolline, pink)
+    dude = RectangleAsset(30, 30, redline, green)
     def __init__(self, x, y):
         super().__init__(Dude.dude, (x, y))
         self.x = x
@@ -72,10 +72,10 @@ class Platformer(App):
         self.listenKeyEvent('keydown', 'p', self.buildDude)
         self.listenKeyEvent('keydown', 'w', self.buildWall)
         self.listenMouseEvent('mousemove', self.motion)
-        self.listenKeyEvent('keydown', 'right arrow', self.moveDudeR)
-        self.listenKeyEvent('keydown', 'left arrow', self.moveDudeL)
-        self.listenKeyEvent('keydown', 'up arrow', self.moveDudeU)
-        self.listenKeyEvent('keydown', 'down arrow', self.moveDudeD)
+        self.listenKeyEvent('keydown', 'right arrow', self.R)
+        self.listenKeyEvent('keydown', 'left arrow', self.L)
+        self.listenKeyEvent('keydown', 'up arrow', self.U)
+        self.listenKeyEvent('keydown', 'down arrow', self.D)
     
     def motion(self, event):
         self.mousex = event.x
@@ -93,20 +93,25 @@ class Platformer(App):
         y = self.mousey - self.mousey%40
         Wall(x-10, y-10)
         
+    def destroyWall(self, event):
+        x = self.mousex - self.mousex%40
+        y = self.mousey - self.mousey%40
+        Wall(x-10, y-10)
         
-    def moveDudeR(self, event):
+        
+    def R(self, event):
         self.dudesprite.x += 5
         collisions = self.dudesprite.collidingWithSprites(Wall)
         if collisions:
             self.dudesprite.x -= 5
             
-    def moveDudeL(self, event):
+    def L(self, event):
         self.dudesprite.x -= 5
         collisions = self.dudesprite.collidingWithSprites(Wall)
         if collisions:
             self.dudesprite.x += 5
             
-    def moveDudeU(self, event):
+    def U(self, event):
         global gravity
         if gravity == 0:
             gravity = -7
@@ -114,7 +119,7 @@ class Platformer(App):
             if collisions:
                 self.dudesprite.y += 50
 
-    def moveDudeD(self, event):
+    def D(self, event):
         self.dudesprite.y += 5
         collisions = self.dudesprite.collidingWithSprites(Wall)
         if collisions:
