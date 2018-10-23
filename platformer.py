417567c2cbdeeed6da3ec08cee3468e956080392
tristanmeyer <42872117+tristanmeyer@users.noreply.github.com>
2018-10-23 17:56:20 -0400

    Updated from Brython Server: 10/23/2018 5:56:20 PM

--- platformer.py
+++ platformer.py
@@ -37,6 +37,7 @@ grid=RectangleAsset(50,50,gridline,white)
 Dimensions of Workspace = 1000 - 800
 Number of Blocks in Dimensions = 25 - 15
 """
+#==Grid=========================================================================
 x = 0 
 y = 0 
 for b in range(15):
@@ -46,12 +47,23 @@ for b in range(15):
     x = 0
     Sprite(grid, (x,y))
     y = y + 50
-   
-
     
-
-
-
-
+#==Player=======================================================================  
 myapp = App()
-myapp.run()
+
+playerasset = RectangleAsset(15, 35, noline, green)
+player = Sprite(playerasset)
+player.direction = 1
+
+
+def forward(a):
+    player.x += player.direction
+
+myapp.listenKeyEvent('keydown','d', forward)
+
+def backward(b):
+    player.x -= player.direction
+
+myapp.listenKeyEvent('keydown','a', backward)
+
+myapp.run(player.x)
