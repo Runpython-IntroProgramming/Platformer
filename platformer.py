a32463c36d3a336055b7a064d9d0cffeed2e3c10
johari2020 <42872004+johari2020@users.noreply.github.com>
2018-11-05 11:42:43 -0500

    Updated from Brython Server: 11/5/2018 11:42:42 AM

--- platformer.py
+++ platformer.py
@@ -18,7 +18,7 @@ pink = Color(0xFF00FF, 1.0)
 red = Color(0xFF5733, 1.0)
 white = Color(0xFFFFFF, 1.0)
 red = Color(0xffbaf0, 1.0)
-green = Color(0x00ff00, 1.0)
+green = Color(0x9cf5f0, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 white = Color(0xffffff, 1.0)
@@ -37,21 +37,53 @@ grid=RectangleAsset(30,30,gridline,white)
 
 rectangle = RectangleAsset(50, 20, thinline, blue)
 Sprite(rectangle) 
-class Platform(Sprite):
+
+class Wall(Sprite):
     
     asset = RectangleAsset(35, 35, thinline, red)
 
     def __init__(self, position):
-        super().__init__(Platform.asset, position)
+        super().__init__(Wall.asset, position)
+
+class Player(Sprite):
+    
+    asset = RectangleAsset(10, 20, thinline, blue)
+
+    def __init__(self, position):
+        super().__init__(Player.asset, position)
         
+class Spring(Sprite):
+    
+    asset = RectangleAsset(10, 5, thinline, red)
+
+    def __init__(self, position):
+        super().__init__(Spring.asset, position)
 class Platformer(App):
     
     def __init__(self):
         super().__init__()
-        bg_asset = RectangleAsset(1000, 800, noline, purple)
+        bg_asset = RectangleAsset(1000, 800, noline, green)
         bg = Sprite(bg_asset, (0,0))
-        Platform((100,100))
-
+        Wall((100,100))
+        self.asset = [0,0] 
+        Platformer.listenMouseEvent('mousemove', self.mouse) 
+        Platformer.listenKeyEvent('keydown', 'w', self.wall)
+        Platformer.listenKeyEvent('keydown', 's', self.spring)
+        Platformer.listenKeyEvent('keydown', 'p', self.player)
         
+    def mouse(self, event):
+        self.asset[0]= event.x
+        self.asset[1] = event.y
+    
+    def wall(self, event):
+        Wall((self.asset[0], self.asset[1]))
+    
+    def player(self, event):
+        Player((self.asset[0], self.asset[1]))
+    
+    def spring(self, event):
+        Spring((self.asset[0], self.asset[1]))
+
+
 myapp = Platformer()
 myapp.run()
