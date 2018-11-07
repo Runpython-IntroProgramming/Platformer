9a26dba56ce52b70f057bdb763189d959fc96abc
oliviaosimon <42871888+oliviaosimon@users.noreply.github.com>
2018-11-07 11:43:23 -0500

    Updated from Brython Server: ‎11‎/‎7‎/‎2018 ‎11‎:‎43‎:‎24‎ ‎AM

--- platformer.py
+++ platformer.py
@@ -13,14 +13,14 @@ myapp = App()
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
 
-blue = Color(0x2EFEC8, 1.0)
+lightBlue = Color(0x2EFEC8, 1.0)
 black = Color(0x000000, 1.0)
 pink = Color(0xFF00FF, 1.0)
 red = Color(0xFF5733, 1.0)
 white = Color(0xFFFFFF, 1.0)
 red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
-blue = Color(0x0000ff, 1.0)
+darkBlue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 white = Color(0xffffff, 1.0)
 grey = Color(0xC0C0C0, 1.0)
@@ -103,7 +103,7 @@ class Playah(Newton):
     def __init__(self, x, y, app):
         w = 10 
         h = 20
-        super().__init__(x-w//2, y-h//2, w, h, green, app)
+        super().__init__(x-w//2, y-h//2, w, h, lightBlue, app)
     def move(self, key):
         if key == "left arrow":
             if self.vx > 0:
