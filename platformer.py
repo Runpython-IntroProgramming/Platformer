88a19e38e034a4de002cf8e11350d2df5c71d769
oliviaosimon <42871888+oliviaosimon@users.noreply.github.com>
2018-11-14 11:24:21 -0500

    Updated from Brython Server: 11/14/2018 11:24:21 AM

--- platformer.py
+++ platformer.py
@@ -197,7 +197,7 @@ class Playah(Newton):
                 self.vx = 0
 
   
-# A spring makes the player "bounce" higher than she can jump
+# 
 class Spring(Newton):
     def __init__(self, x, y, app):
         w = 10
