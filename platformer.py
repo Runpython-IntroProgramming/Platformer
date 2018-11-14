995ce7789499daf5bea74be19733d26d456d0b17
oliviaosimon <42871888+oliviaosimon@users.noreply.github.com>
2018-11-14 11:45:21 -0500

    Updated from Brython Server: 11/14/2018 11:45:21 AM

--- platformer.py
+++ platformer.py
@@ -61,8 +61,8 @@ class Newton(Sprite):
     def __init__(self, x, y, width, height, color, app):
         self.vx = self.vy = 0
         self.stuck = False
-        self.app = app                          # app, need to know
-        self.resting = False                    # whether resting on wall
+        self.app = app                      
+        self.resting = False                    
         super().__init__(
             RectangleAsset(
                 width, height, 
