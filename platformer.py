bb23086b5ac095f67f1028366f6f1bd70585cd6f
oliviaosimon <42871888+oliviaosimon@users.noreply.github.com>
2018-12-03 11:47:32 -0500

    Updated from Brython Server: ‎12‎/‎3‎/‎2018 ‎11‎:‎47‎:‎36‎ ‎AM

--- platformer.py
+++ platformer.py
@@ -20,336 +20,177 @@ from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, El
 
 myapp = App()
 
-
-
 SCREEN_WIDTH = 1000
-
 SCREEN_HEIGHT = 800
 
-
-
 lightBlue = Color(0x2EFEC8, 1.0)
-
 black = Color(0x000000, 1.0)
-
 pink = Color(0xFF00FF, 1.0)
-
 red = Color(0xFF5733, 1.0)
-
 white = Color(0xFFFFFF, 1.0)
-
 red = Color(0xff0000, 1.0)
-
 green = Color(0x00ff00, 1.0)
-
 darkBlue = Color(0x0000ff, 1.0)
-
 black = Color(0x000000, 1.0)
-
 white = Color(0xffffff, 1.0)
-
 grey = Color(0xC0C0C0, 1.0)
 
-
-
 thinline = LineStyle(2, black)
-
 blkline = LineStyle(1, black)
-
 noline = LineStyle(0, white)
-
 coolline = LineStyle(1, grey)
-
 blueline = LineStyle(2, darkBlue)
-
 redline = LineStyle(1, red)
-
 greenline = LineStyle(1, green)
-
 gridline = LineStyle(1, grey)
-
 grid=RectangleAsset(30,30,gridline,white)
-
 
 
 class Blocks(Sprite):
 
     def __init__(self, x, y, w, h, color):
-
         grid = lambda X : X - X % w
-
         super().__init__(
-
             RectangleAsset(w-1, h-1, LineStyle(0,Color(0, 1.0)), color),(grid(x), grid(y)))
-
         #collisions
-
         collisioncontra =self.collidingWithSprites(type(self))
-
         if len(collisioncontra):
-
             collisioncontra[0].destroy()
-
-            
-
 
 
 class Wall(Blocks):
-
     def __init__(self,x,y):
-
         super().__init__(x,y,60,60,grey)      #(self, x, y, w, h, color)
 
-        
-
 class Newton(Sprite):
-
     def __init__(self, x, y, width, height, color, app):
-
         self.vx = self.vy = 0
-
         self.stuck = False
-
-        self.app = app                          # app, need to know
-
-        self.resting = False                    # whether resting on wall
-
+        self.app = app                         
+        self.resting = False                   
         super().__init__(
-
             RectangleAsset(
-
                 width, height, 
-
                 LineStyle(0, black),
-
                 color), (x, y)) 
-
                 
-
         collisioncontra=self.collidingWithSprites()
-
         if len(collisioncontra):
-
             self.destroy()
 
     def step(self):
-
         # vertical movements
-
         self.y += self.vy
-
         collides = self.collidingWithSprites(Wall)
-
         collides.extend(self.collidingWithSprites(Platform))
-
         for collider in collides:
-
             if self.vy > 0 or self.vy < 0:
-
                 if self.vy > 0:
-
                     self.y = collider.y - self.height - 1
-
                     if not self.resting:
-
                         self.vx = 0
-
                     self.resting = True
-
                     self.vy = 0
-
                 # upward collisions for true Wall only
-
                 elif isinstance(collider, Wall):
-
                     self.y = collider.y + collider.height
-
                     self.vy = 0
 
         # process movement in horizontal direction second
-
         self.x += self.vx
-
         collides = self.collidingWithSprites(Wall)
-
         collides.extend(self.collidingWithSprites(Game))
-
         for collider in collides:
-
             if self.vx > 0 or self.vx < 0:
-
                 if self.vx > 0:
-
                     self.x = collider.x - self.width
-
                 else:
-
                     self.x = collider.x + collider.width
-
                 self.vx = 0
-
         # adjust vertical velocity for acceleration due to gravity
-
         self.vy += 1
-
         # check for out of bounds
-
         if self.y > self.app.height:
-
             self.app.killMe(self)
 
-
-
 class Playah(Newton):
-
     def __init__(self, x, y, app):
-
         w = 10 
-
         h = 20
-
         super().__init__(x-w//2, y-h//2, w, h, lightBlue, app)
-
     def move(self, key):
-
         if key == "left arrow":
-
             if self.vx > 0:
-
                 self.vx = 0
-
             else:
-
                 self.vx = -5
-
         elif key == "right arrow":
-
             if self.vx < 0:
-
                 self.vx = 0
-
             else:
-
                 self.vx = 5
-
         elif key == "up arrow" and self.resting:
-
             self.vy = -10
-
             self.resting = False
-
             
-
     def stopMove(self, key):
-
         if key == "left arrow" or key == "right arrow":
-
             if self.resting:
-
                 self.vx = 0
-
-
 
 def wallKey(event):
-
     Sprite(Wall,)
-
-
-
-
-
 #Index/Glossarix
 
 class Game(App):
-
     def __init__(self):
-
         super().__init__()
-
         self.p = None
-
         self.pos = (0,0)
-
         self.listenKeyEvent("keydown", "w", self.newWall)
-
         self.listenKeyEvent("keydown", "p", self.newPlayah)
-
         self.listenKeyEvent("keydown", "left arrow", self.moveKey)
-
         self.listenKeyEvent("keydown", "right arrow", self.moveKey)
-
         self.listenKeyEvent("keydown", "up arrow", self.moveKey)
-
         self.listenKeyEvent("keyup", "left arrow", self.stopMoveKey)
-
         self.listenKeyEvent("keyup", "right arrow", self.stopMoveKey)
-
         self.listenKeyEvent("keyup", "up arrow", self.stopMoveKey)
-
         self.listenMouseEvent("mousemove", self.moveMouse)
-
         self.KillList = []
-
-
-
     def moveMouse(self, event):
-
         self.pos = (event.x, event.y)
 
-    
-
     def newWall(self, event):
-
         Wall(self.pos[0], self.pos[1])
 
-        
-
     def newPlayah(self, event):
-
         for p in Game.getSpritesbyClass(Playah):
-
             p.destroy()
-
             self.p = None
-
         self.p = Playah(self.pos[0], self.pos[1], self)
 
-        
-
     def moveKey(self, event):
-
         if self.p:
-
             self.p.move(event.key)
-
         
-
     def stopMoveKey(self, event):
-
         if self.p:
-
             self.p.stopMove(event.key)
 
-            
-
     def killMe(self, obj):
-
         if obj in self.FallingSprings:
-
             self.FallingSprings.remove(obj)
-
         elif obj == self.p:
-
             self.p = None
-
         if not obj in self.KillList:
-
             self.KillList.append(obj)
 
-
-
-
-
 app = Game()
-
 app.run()
+
+
+
+
+
+
+
