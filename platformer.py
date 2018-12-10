da54c790803bd2875510ef1fa3976b9e49b32242
kylerozzi <42872056+kylerozzi@users.noreply.github.com>
2018-12-10 15:07:40 -0500

    Updated from Brython Server: 12/10/2018 3:07:40 PM

--- platformer.py
+++ platformer.py
@@ -8,8 +8,30 @@ https://github.com/HHS-IntroProgramming/Platformer
 """
 import pygame as pg
 
+blue = Color(0x2EFEC8, 1.0)
+black = Color(0x000000, 1.0)
+pink = Color(0xFF00FF, 1.0)
+red = Color(0xFF5733, 1.0)
+white = Color(0xFFFFFF, 1.0)
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+white = Color(0xffffff, 1.0)
+grey = Color(0xC0C0C0, 1.0)
+
+thinline = LineStyle(2, black)
+blkline = LineStyle(1, black)
+noline = LineStyle(0, white)
+coolline = LineStyle(1, grey)
+blueline = LineStyle(2, blue)
+redline = LineStyle(1, red)
+greenline = LineStyle(1, green)
+gridline = LineStyle(1, grey)
+grid=RectangleAsset(30,30,gridline,white)
+
 class Platformer:
-    def __init(self):
+    def __init__(self):
         pygame.init()
         pygame.mixer.init()
         self.screen=pygame.set_mode(WIDTH,HEIGHT))
@@ -24,12 +46,14 @@ class Platformer:
         self.update()
         self.draw()
     def update(self):
-        pass
+        self.all_sprites.update()
     def events(self):
         if event.type==pg.QUIT:
             running=False
     def draw(self):
-        pass
+        screen.fill(white)
+        all_sprites.draw(screen)
+        pg.display.flip()
         
 g=Platformer()
 
