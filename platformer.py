42f3483aa9fe6129e5a6cf742dd1db9389d3aa96
mgardner3901 <31485789+mgardner3901@users.noreply.github.com>
2018-12-10 12:28:04 -0500

    Updated from Brython Server: 12/10/2018 12:28:05 PM

--- platformer.py
+++ platformer.py
@@ -6,45 +6,33 @@ Assignment:
 Write and submit a program that implements the sandbox platformer game:
 https://github.com/HHS-IntroProgramming/Platformer
 """
-from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset, Frame
 
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 800
+import pygame
 
-blue = Color(0x2EFEC8, 1.0)
-black = Color(0x000000, 1.0)
-pink = Color(0xFF00FF, 1.0)
-red = Color(0xFF5733, 1.0)
-white = Color(0xFFFFFF, 1.0)
-red = Color(0xff0000, 1.0)
-green = Color(0x00ff00, 1.0)
-blue = Color(0x0000ff, 1.0)
-black = Color(0x000000, 1.0)
-white = Color(0xffffff, 1.0)
-grey = Color(0xC0C0C0, 1.0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
-thinline = LineStyle(2, black)
-blkline = LineStyle(1, black)
-noline = LineStyle(0, white)
-coolline = LineStyle(1, grey)
-blueline = LineStyle(2, blue)
-redline = LineStyle(1, red)
-greenline = LineStyle(1, green)
-gridline = LineStyle(1, grey)
-grid=RectangleAsset(30,30,gridline,white)
-
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+ 
+ 
 class Player(pygame.sprite.Sprite):
-    
-    def __init__(self):
-        
-        super().__init__()
 
+    def __init__(self):
+ 
+
+        super().__init__()
+ 
         width = 40
         height = 60
         self.image = pygame.Surface([width, height])
         self.image.fill(RED)
  
         self.rect = self.image.get_rect()
+
         self.change_x = 0
         self.change_y = 0
  
@@ -58,17 +46,18 @@ class Player(pygame.sprite.Sprite):
  
         block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
         for block in block_hit_list:
-           
+          
             if self.change_x > 0:
                 self.rect.right = block.rect.left
             elif self.change_x < 0:
                 self.rect.left = block.rect.right
- 
+
         self.rect.y += self.change_y
  
+
         block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
         for block in block_hit_list:
-            
+ 
             if self.change_y > 0:
                 self.rect.bottom = block.rect.top
             elif self.change_y < 0:
@@ -77,7 +66,6 @@ class Player(pygame.sprite.Sprite):
             self.change_y = 0
  
     def calc_grav(self):
-
         if self.change_y == 0:
             self.change_y = 1
         else:
@@ -86,42 +74,61 @@ class Player(pygame.sprite.Sprite):
         if self.rect.y >= SCREEN_HEIGHT - self.rect.height and self.change_y >= 0:
             self.change_y = 0
             self.rect.y = SCREEN_HEIGHT - self.rect.height
-            
-            
-class Level(object):
+ 
+    def jump(self):
+ 
+        self.rect.y += 2
+        platform_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        self.rect.y -= 2
+ 
+        if len(platform_hit_list) > 0 or self.rect.bottom >= SCREEN_HEIGHT:
+            self.change_y = -10
+ 
+    def go_left(self):
+        self.change_x = -6
+ 
+    def go_right(self):
+        self.change_x = 6
+ 
+    def stop(self):
+        self.change_x = 0
+ 
+ 
+class Platform(pygame.sprite.Sprite):
+ 
+    def __init__(self, width, height):
+            code.
  
     def __init__(self, player):
-   
+    
         self.platform_list = pygame.sprite.Group()
         self.enemy_list = pygame.sprite.Group()
         self.player = player
-         
+
         self.background = None
  
-    def update(self):
         self.platform_list.update()
         self.enemy_list.update()
  
     def draw(self, screen):
-
-        screen.fill(BLUE)
-
+    
+    screen.fill(BLUE)
+ 
         self.platform_list.draw(screen)
         self.enemy_list.draw(screen)
  
- 
+
 class Level_01(Level):
  
     def __init__(self, player):
  
-
         Level.__init__(self, player)
 
         level = [[210, 70, 500, 500],
                  [210, 70, 200, 400],
                  [210, 70, 600, 300],
                  ]
- 
+
         for platform in level:
             block = Platform(platform[0], platform[1])
             block.rect.x = platform[2]
@@ -131,7 +138,7 @@ class Level_01(Level):
  
  
 def main():
-   
+
     pygame.init()
  
     size = [SCREEN_WIDTH, SCREEN_HEIGHT]
@@ -141,7 +148,7 @@ def main():
  
     player = Player()
  
-    level_list = []
+     level_list = []
     level_list.append( Level_01(player) )
  
     current_level_no = 0
@@ -153,14 +160,49 @@ def main():
     player.rect.x = 340
     player.rect.y = SCREEN_HEIGHT - player.rect.height
     active_sprite_list.add(player)
-
+ 
     done = False
-
-    
+ 
     clock = pygame.time.Clock()
  
+    # -------- Main Program Loop -----------
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
  
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.go_left()
+                if event.key == pygame.K_RIGHT:
+                    player.go_right()
+                if event.key == pygame.K_UP:
+                    player.jump()
+ 
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT and player.change_x < 0:
+                    player.stop()
+                if event.key == pygame.K_RIGHT and player.change_x > 0:
+                    player.stop()
+ 
+        active_sprite_list.update()
+ 
+        current_level.update()
+ 
+        if player.rect.right > SCREEN_WIDTH:
+            player.rect.right = SCREEN_WIDTH
+ 
+        if player.rect.left < 0:
+            player.rect.left = 0
+ 
+        current_level.draw(screen)
+        active_sprite_list.draw(screen)
+ 
+        clock.tick(60)
+ 
+        pygame.display.flip()
+ 
+        pygame.quit()
+ 
+if __name__ == "__main__":
+main()
