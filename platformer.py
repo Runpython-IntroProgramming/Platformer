7f8659b1d9ce0c2d0d5c715a755bb2bbdc96ef88
kylerozzi <42872056+kylerozzi@users.noreply.github.com>
2018-12-11 17:24:59 -0500

    Updated from Brython Server: 12/11/2018 5:24:58 PM

--- platformer.py
+++ platformer.py
@@ -35,11 +35,11 @@ gridline = LineStyle(1, grey)
 grid = RectangleAsset(40,40,gridline,white)
 
 class Block(Sprite):
-    def __init__(self, position):
-        box = RectangleAsset(40,40, cooline, red)
+    def __init__(self, x, y, w, h, color):
+        box = RectangleAsset(40,40,LineStyle(4,grey), red)
         x-=x%w
         y-=y%w
-        super().__init__(box, (x,y))
+        super().__init__(box,x,y)
 
 
 class Falling(Sprite):
@@ -53,22 +53,32 @@ class Falling(Sprite):
     def step(self):
         self.x=+self.vx
         collision=self.collidingWithSprites(Block)
+        if self.x>self.app.w:
+            self.app.killMe(self)
         for i in collision:
             if self.vx>0 or self.vx<0:
                 if self.vx<0:
-                    self.x=i.x+i.w+1
+                    self.x=i.x+i.w+.1
                 else:
-                    self.x=i.x-self.w-1
+                    self.x=i.x-self.w-.1
                 self.vx=0
         self.y=+self.vy
         collision1=self.collidingWithSprites(Block)
-        for i in collision:
-            if self.vx>0 or self.vx<0:
-                if self.vx<0:
-                    self.x=i.x+i.w+1
+        self.vy+=0.8
+        if self.y>self.app.h:
+            self.app.killMe(self)
+        for i in collision1:
+            if self.vy>0 or self.vy<0:
+                if self.vy>0:
+                    self.y= i.y + self.h-.1
+                    if not self.resting:
+                        self.vx=0
+                    self.resting=True
                 else:
-                    self.x=i.x-self.w-1
-                self.vx=0
+                    self.y=i.y+i.h
+                    self.vy=0
 
-
+class Spring(Falling):
+    def __init__(self,x,y,app):
+        super().__init__(x,y,10,10,pink, app)
 
