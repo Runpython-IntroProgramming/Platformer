b3d5d1fe199f9ff7015232a5e87e3964aa969c60
kylerozzi <42872056+kylerozzi@users.noreply.github.com>
2018-12-10 20:34:14 -0500

    Updated from Brython Server: 12/10/2018 8:34:14 PM

--- platformer.py
+++ platformer.py
@@ -6,19 +6,21 @@ Assignment:
 Write and submit a program that implements the sandbox platformer game:
 https://github.com/HHS-IntroProgramming/Platformer
 """
-import pygame as pg
+import pygame
 
-blue = Color(0x2EFEC8, 1.0)
-black = Color(0x000000, 1.0)
-pink = Color(0xFF00FF, 1.0)
-red = Color(0xFF5733, 1.0)
-white = Color(0xFFFFFF, 1.0)
-red = Color(0xff0000, 1.0)
-green = Color(0x00ff00, 1.0)
-blue = Color(0x0000ff, 1.0)
-black = Color(0x000000, 1.0)
-white = Color(0xffffff, 1.0)
-grey = Color(0xC0C0C0, 1.0)
+
+WIDTH=1600
+HEIGHT=800
+FPS=30
+
+black=(0,0,0)
+white=(255,255,255)
+red=(255,0,0)
+green=(0,255,0)
+blue=(0,0,255)
+yellow=(255,255,0)
+pink=(255,0,255)
+lblue=(0,255,255)
 
 thinline = LineStyle(2, black)
 blkline = LineStyle(1, black)
@@ -33,28 +35,38 @@ grid=RectangleAsset(30,30,gridline,white)
 class Platformer:
     def __init__(self):
         pygame.init()
-        pygame.mixer.init()
         self.screen=pygame.set_mode(WIDTH,HEIGHT))
+        self.clock=pygame.time.Clock()
         self.running=True
     def new(self):
-        self.all_sprites=pg.sprite.Group()
-        self.player=P1()
-        self.all_sprites.add(self.player)
+        self.all_sprites=pygame.sprite.Group()
+        self.P1=Player()
+        self.all_sprites.add(self.P1)
         self.run()
     def run(self):
-        self.events()
-        self.update()
-        self.draw()
+        self.playing=True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
     def update(self):
         self.all_sprites.update()
     def events(self):
-        if event.type==pg.QUIT:
-            running=False
+        for i in pygame.event.get():
+            if event.type==pygame.QUIT:
+                if self.playing:
+                    self.playing=False
+                self.running=False
     def draw(self):
         screen.fill(white)
-        all_sprites.draw(screen)
-        pg.display.flip()
-        
-g=Platformer()
+        self.all_sprites.draw(self.screen)
+        pygame.display.flip()
+    def ssc(self):
+        pass
+    def esc(self):
+        pass
+p=Platformer()
+
 
 
