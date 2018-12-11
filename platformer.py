932d6593b6fbb4fb1b41028779a66df017ae5893
mgardner3901 <31485789+mgardner3901@users.noreply.github.com>
2018-12-11 12:55:50 -0500

    Updated from Brython Server: 12/11/2018 12:55:50 PM

--- platformer.py
+++ platformer.py
@@ -10,16 +10,15 @@ https://github.com/HHS-IntroProgramming/Platformer
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset, Frame
 
 blue = Color(0x2EFEC8, 1.0)
+yellow = Color(0xffff00, 1.0)
 black = Color(0x000000, 1.0)
 pink = Color(0xFF00FF, 1.0)
 red = Color(0xFF5733, 1.0)
-white = Color(0xFFFFFF, 1.0)
 red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
-black = Color(0x000000, 1.0)
-white = Color(0xffffff, 1.0)
-grey = Color(0xC0C0C0, 1.0)
+
+
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
@@ -40,7 +39,7 @@ class Wall(GenericWall):
         
 class Goal(GenericWall):
     def __init__(self, x, y):
-        super().__init__(x, y, 40, 40, Color(0xff0000, 1.0))
+        super().__init__(x, y, 40, 80, Color(0xffff00, 1.0))
 
 class Platform(GenericWall):
     def __init__(self, x, y):
