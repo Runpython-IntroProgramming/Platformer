db196b33daac77beda8c6db724f71b84bb75af95
kylerozzi <42872056+kylerozzi@users.noreply.github.com>
2018-12-11 11:49:04 -0500

    Updated from Brython Server: 12/11/2018 11:49:03 AM

--- platformer.py
+++ platformer.py
@@ -6,172 +6,28 @@ Assignment:
 Write and submit a program that implements the sandbox platformer game:
 https://github.com/HHS-IntroProgramming/Platformer
 """
-import pygame
-import random
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset, Frame
 
-WIDTH=800
-HEIGHT=600
-FPS=60
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
 
-black=(0,0,0)
-white=(255,255,255)
-red=(255,0,0)
-green=(0,255,0)
-blue=(0,0,255)
-yellow=(255,255,0)
-pink=(255,0,255)
-lblue=(0,255,255)
+blue = Color(0x2EFEC8, 1.0)
+black = Color(0x000000, 1.0)
+pink = Color(0xFF00FF, 1.0)
+red = Color(0xFF5733, 1.0)
+white = Color(0xFFFFFF, 1.0)
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+white = Color(0xffffff, 1.0)
+grey = Color(0xC0C0C0, 1.0)
 
-vector=pygame.math.Vector2
-fric = -0.13
-pacc = 1
-pgrav = 0.8
-
-platl = [(0, HEIGHT - 40, WIDTH, 40)]
-
-class User(pygame.sprite.Sprite):
-    def __init__(self, game):
-        pygame.sprite.Sprite.__init__(self)
-        self.game = game
-        self.image=pygame.Surface((20,40))
-        self.image.fill(lblue)
-        self.rect=self.image.get_rect()
-        self.rect.center=(WIDTH / 2, HEIGHT / 2)
-        self.pos=vector(WIDTH / 2, HEIGHT / 2)
-        self.vel=vector(0,0)
-        self.acc=vector(0,0)
-    def jump(self):
-        self.rect.x += 1
-        collide = pygame.sprite.spritecollide(self, self.game.platforms, False)
-        self.rect.x-=1
-        if collide:
-            self.vel.y=-20
-    def update(self):
-        self.acc=vector(0,pgrav)
-        arrows=pygame.key.get_pressed()
-        if arrows[pygame.K_LEFT]:
-            self.acc.x= -pacc
-        if arrows[pygame.K_RIGHT]:
-            self.acc.x= pacc
-        self.acc.x += self.vel.x * fric
-        self.vel +=self.acc
-        self.pos += self.vel + 0.8 * self.acc
-        if self.pos.x > WIDTH:
-            self.pos.x = 0
-        if self.pos.x < 0:
-            self.pos.x = WIDTH
-
-        self.rect.midbottom = self.pos
-
-class Wall(pygame.sprite.Sprite):
-    def __init__(self, x, y, w, h):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((w, h))
-        self.image.fill(black)
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-
-class Platformer:
-    def __init__(self):
-        pygame.init()
-        self.screen=pygame.display.set_mode((WIDTH,HEIGHT))
-        self.clock=pygame.time.Clock()
-        self.running=True
-    def new(self):
-        self.asp=pygame.sprite.Group()
-        self.platforms=pygame.sprite.Group()
-        self.player= User(self)
-        self.platforms=pygame.sprite.Group()
-        self.asp.add(self.player)
-        for plat in platl:
-            q = Wall(*plat)
-            self.asp.add(q)
-            self.platforms.add(q)
-        self.run()
-    def run(self):
-        self.playing=True
-        while self.playing:
-            self.clock.tick(FPS)
-            self.events()
-            self.update()
-            self.draw()
-    def update(self):
-        self.asp.update()
-        collide=pygame.sprite.spritecollide(self.player, self.platforms, False)
-        if collide:
-            self.player.pos.y=collide[0].rect.top
-            self.player.vel.y=0
-    def events(self):
-        for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                if self.playing:
-                    self.playing=False
-            self.running=False
-            if event.type==pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    self.player.jump()
-    def draw(self):
-        self.screen.fill(white)
-        self.asp.draw(self.screen)
-        pygame.display.flip()
-    def ssc(self):
-        pass
-    def esc(self):
-        pass
-p=Platformer()
-p.ssc()
-while p.running:
-    p.new()
-    p.esc()
-pygame.quit()
-
-
-class Platformer:
-    def __init__(self):
-        pygame.init()
-        self.screen=pygame.display.set_mode((WIDTH,HEIGHT))
-        self.clock=pygame.time.Clock()
-        self.running=True
-    def new(self):
-        self.asp=pygame.sprite.Group()
-        self.player= User()
-        self.Wall=pygame.sprite.Group()
-        self.asp.add(self.player)
-        b=Wall(0,HEIGHT-40, WIDTH,80)
-        self.asp.add(b)
-        self.Wall.add(b)
-        self.run()
-    def run(self):
-        self.playing=True
-        while self.playing:
-            self.clock.tick(FPS)
-            self.events()
-            self.update()
-            self.draw()
-    def update(self):
-        self.asp.update()
-        collide=pygame.sprite.spritecollide(self.player, self.Wall, False)
-        if collide:
-            self.player.pos.y=collide[0].rect.top
-            self.player.vel.y=0
-    def events(self):
-        for i in pygame.event.get():
-            if i.type==pygame.QUIT:
-                if self.playing:
-                    self.playing=False
-            self.running=False
-    def draw(self):
-        self.screen.fill(white)
-        self.asp.draw(self.screen)
-        pygame.display.flip()
-    def ssc(self):
-        pass
-    def esc(self):
-        pass
-p=Platformer()
-p.ssc()
-while p.running:
-    p.new()
-    p.esc()
-pygame.quit()
+thinline = LineStyle(2, black)
+blkline = LineStyle(1, black)
+noline = LineStyle(0, white)
+coolline = LineStyle(1, grey)
+blueline = LineStyle(2, blue)
+redline = LineStyle(1, red)
+greenline = LineStyle(1, green)
+gridline = LineStyle(1, grey)
