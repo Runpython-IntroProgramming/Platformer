dd551d33496a0e7794f13b7be75be5765444e101
mgardner3901 <31485789+mgardner3901@users.noreply.github.com>
2018-12-12 08:07:12 -0500

    Updated from Brython Server: 12/12/2018 8:07:13 AM

--- platformer.py
+++ platformer.py
@@ -26,13 +26,27 @@ green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 
 
-thinline = LineStyle(2, black)
-blkline = LineStyle(1, black)
-noline = LineStyle(0, white)
-coolline = LineStyle(1, grey)
-blueline = LineStyle(2, blue)
-redline = LineStyle(1, red)
-greenline = LineStyle(1, green)
-gridline = LineStyle(1, grey)
-grid=RectangleAsset(30,30,gridline,white)
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+
+
+
+class GenericWall(Sprite):
+   def __init__(self, x, y, w, h, color):
+       snapfunc = lambda X : X - X % w
+       super().__init__(
+           RectangleAsset(w-1,h-1,LineStyle(0,Color(0x0000ff, 1.0)), color),
+           (snapfunc(x), snapfunc(y)))
+       collideswith = self.collidingWithSprites(type(self))
+       if len(collideswith):
+           collideswith[0].destroy()
+
+class Wall(GenericWall):
+   def __init__(self, x, y):
+       super().__init__(x, y, 35, 35, Color(0x0000ff, 1.0))
+      
+class Goal(GenericWall):
+   def __init__(self, x, y):
+       super().__init__(x, y, 40, 80, Color(0xffff00, 1.0))
+
 
