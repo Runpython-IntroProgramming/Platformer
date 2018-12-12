161a688893fd6e2f42f4ac059031e3a51502975e
kylerozzi <42872056+kylerozzi@users.noreply.github.com>
2018-12-12 03:05:45 -0500

    Updated from Brython Server: 12/12/2018 3:05:44 AM

--- platformer.py
+++ platformer.py
@@ -8,10 +8,8 @@ https://github.com/HHS-IntroProgramming/Platformer
 """
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset, Frame
 from math import floor
-
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
-
 pink = Color(0xFF00FF, 1.0)
 red = Color(0xFF5733, 1.0)
 red = Color(0xff0000, 1.0)
@@ -19,9 +17,7 @@ blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 white = Color(0xffffff, 1.0)
 grey = Color(0xC0C0C0, 1.0)
-
 gridline = LineStyle(1, grey)
-
 class Block(Sprite):
     def __init__(self, position):
         box = RectangleAsset(40,40, LineStyle(4, grey),red)
@@ -158,7 +154,3 @@ print('create a player with p')
 print('Control your player with the arrow keys')
 app=Game()
 app.run()
-                
-         
-                
-    
